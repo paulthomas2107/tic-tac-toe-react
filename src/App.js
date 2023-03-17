3bba34d30373577e441814f58da9aeab3bfe4b64
Paul Thomas <paulthomas2107@gmail.com>
2023-03-17 16:06:52 +0000

    More

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Cell from './components/Cell';
 
 const App = () => {
   const [cells, setCells] = useState(['', '', '', '', '', '', '', '', '']);
+  const [go, setGo] = useState('circle');
+  const [winningMessage, setWinningMessage] = useState(null);
+
+  const message = 'It is now ' + go + "'s go";
 
   return (
     <div className="app">
       <div className="gameboard">
         {cells.map((cell, index) => (
-          <Cell key={index} id={index} cell={cell} />
+          <Cell key={index} id={index} cell={cell} setCells={setCells} />
         ))}
       </div>
-      <p></p>
+      <p>{message}</p>
     </div>
   );
 };
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,15 @@
-const Cell = () => {
-  return <div className="square"></div>;
+const Cell = ({ id, cell, setCells }) => {
+  const handleClick = (e) => {
+    const taken =
+      e.target.firstChild.classList.contains('circle') ||
+      e.target.firstChild.classList.contains('cross');
+  };
+
+  return (
+    <div className="square" id={id} onClick={handleClick}>
+      <div className="cross"></div>
+    </div>
+  );
 };
 
 export default Cell;
